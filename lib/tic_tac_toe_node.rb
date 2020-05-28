76ab94564d386d828e0f24951fe770e4e9239d3a
Aharon <aharonzarnighian@gmail.com>
2020-05-28 14:21:15 -0400

    Finished losing_node?... method

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -1,34 +1,59 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-  attr_reader :board
+  attr_reader :board, :next_mover_mark, :prev_move_pos
 
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board, @next_mover_mark, @prev_move_pos = board, next_mover_mark, prev_move_pos
   end
 
   def losing_node?(evaluator)
+    if board.over?
+      if board.winner == next_mover_mark
+        return true
+      elsif board.winner == nil || board.winner == self.get_my_mark
+        return false
+      end
+    end
+
+    if next_mover_mark == evaluator
+      self.children.all? { |child| self.losing_node?(evaluator) }
+    else
+      self.children.any? { |child| self.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
+
+    
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children_nodes = []
+
     @board.rows.each_with_index do |row, idx_r|
       row.each_with_index do |col, idx_c|
-        if col.nil? # (empty)
+        if col.nil?
           new_node = TicTacToeNode.new(@board.dup, @next_mover_mark, prev_move_pos = [idx_r, idx_c])
           new_node.board.rows[idx_r][idx_c] = @next_mover_mark
           self.alternate_mark
+          children_nodes << new_node
         end
       end
     end
+
+    children_nodes
   end
 
   def alternate_mark
-    @next_mover_mark = "X" if @next_mover_mark == "O"
-    @next_mover_mark = "O" if @next_mover_mark == "X"
+    @next_mover_mark = :x if @next_mover_mark == :o
+    @next_mover_mark = :o if @next_mover_mark == :x
+  end
+
+  def get_my_mark
+    return :x if @next_mover_mark == :o
+    return :o if @next_mover_mark == :x 
   end
 end
